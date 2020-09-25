4eac2e32dd7c71ca3d024beea42946e76cf1b173
kanstantsin_karpau <Kanstantsin_Karpau@epam.com>
2020-09-25 03:55:11 +0300

    fix logic errors, remove commented code

--- task1.py
+++ task1.py
@@ -1,25 +1,8 @@
 def check_leap(input_year):
-    if input_year % 4 == 0:
+    if input_year % 4 == 0 and input_year % 100 != 0 and input_year % 400 == 0:
         return True
-    elif input_year % 100 == 0:
-        return False
-    elif input_year % 400 == 0:
-        return True
-    else:
-        return False
+    return False
 
 
-year_to_check = 0
-while year_to_check <= 0:
-    year_to_check = int(input("Input the 'Year' : "))
-    if year_to_check < 0:
-        print("Wrong 'Year' input, try again")
-
-year_checked = check_leap(year_to_check)
-
-# if year_checked:
-#     print("The Year %d is a leap year" % year_to_check)
-# else:
-#     print("The Year %d is NOT a leap year" % year_to_check)
-
-print(year_checked)
+year_to_check = int(input())
+print(check_leap(year_to_check))
--- task2.py
+++ task2.py
@@ -1,25 +1,12 @@
 def remove_duplicates(list1, list2):
     final_list = []
-    duplicate = list1 + list2
-    for num in duplicate:
-        if num not in final_list:
+    for num in list1:
+        if (num in list2) and (num not in final_list):
             final_list.append(num)
     final_list.sort()
     return final_list
 
 
-# list_1 = [3, 9, 91, 102, 4, 5]
-# list_2 = [29, 2, 1]
-
-# hard way to remove duplicates in 2 lists
-list_1 = [int(x) for x in input("Input numbers for list1: ").split()]
-# print(list_1)
-list_2 = [int(x) for x in input("Input numbers for list2: ").split()]
-# print(list_2)
-
+list_1 = list(map(int, input().split()))
+list_2 = list(map(int, input().split()))
 print(remove_duplicates(list_1, list_2))
-
-# another way to remove duplicates
-list_0 = list(set(list_1) | set(list_2))
-list_0.sort()
-print(list_0)
